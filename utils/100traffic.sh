1a229d9ca6f1bb9969ef14a56503a95803f526ec
Aneesh Puttur <aputtur@redhat.com>
2017-07-12 11:27:39 -0400

    Added tags

--- utils/100traffic.sh
+++ utils/100traffic.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
 IP_ADDR=$(ip addr show | grep "inet " | grep 10.19.110.* |  grep -v "127.0.0.1" | awk '{print $2}' | cut -d '/' -f 1)
 for i in {1..100};do
-./proton-sender -t unicast/traffic -l -c -1 -s 1500 -S 1000 -m 1000 -M 1000 -a $IP_ADDR:5672 & 
+./proton-sender -t unicast/traffic -l -c -1 -S 1000 -m 100 -M 100 -a $IP_ADDR:5672 & 
 done
