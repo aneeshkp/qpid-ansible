20a7a3ea0749d1eeff6309df6b516ed3eb6d1ddf
Aneesh Puttur <aputtur@redhat.com>
2017-08-04 09:51:03 -0400

    Added 100instance script

---
+++ utils/100.sh
@@ -0,0 +1,5 @@
+IP_ADDR=$(ip addr show | grep "inet " | grep 10.19.110.* |  grep -v "127.0.0.1" | awk '{print $2}' | cut -d '/' -f 1)
+for i in {1..100};do
+./proton-sender -t unicast/traffic -l -c -1 -m 100 -M 100 -a $IP_ADDR:5672 &
+done
+
